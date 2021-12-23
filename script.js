a55d612434cc8ac82faf1b7ee8eaab42e872f39c
Hugo Navarrete <hugonavarrete.23@gmail.com>
2021-12-23 17:04:46 -0300

    added functionality

--- script.js
+++ script.js
@@ -1,67 +1,132 @@
+const WIN_CONDITIONS = [
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ],
+  [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+  ],
+  [
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ],
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ],
+];
 
-const Player = (playerName, playerMark ) => {
-    const getName = () => playerName;
-    const getMark = () => playerMark;
-
-    return {
-        getName,
-        getMark,
-    }
-
-};
-
+const getPlayer = (playerName, playerMark) => ({
+  getName: () => playerName,
+  getMark: () => playerMark,
+});
 
 const gameFlow = () => {
-    const player1 = Player('player1', 'X');
-    const player2 = Player('player2', 'O');
-    let board = ['', '', '', '', '', '', '', '', ''];
-    const winConditions = [
-        [0, 1, 2],
-        [3, 4 ,5],
-        [6, 7 ,8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    let currentPlayer = player1;
-    const switchPlayers = () => {
-        if(currentPlayer === player1){
-          currentPlayer = player2;
-        } else {
-          currentPlayer = player1;
-        }
-      }    
+  const cells = {};
+  for (let index = 0; index < 9; index++) {
+    cells[index] = document.getElementById(parseInt(index));
+    cells[index].addEventListener("click", () => {
+      addMark(index);
+    });
+  }
 
-    function displayBoard() {
-        for (i = 0; i < 3; i++) {
-            let row = document.createElement('div');
-            row.className = 'row';
-            container.appendChild(row)
-            for (let j = 0; j < 3; j++) {
-                let cell = document.createElement('div');
-                cell.className = 'cell';
-                row.appendChild(cell);
-    
-            }
+  const player1 = getPlayer("player1", "X");
+  const player2 = getPlayer("player2", "O");
+  let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  let currentPlayer = player1;
 
-        }
+  const addMark = (cellId) => {
+    const parsedCell = cellId;
+
+    const boardRow = Math.floor(parsedCell / 3);
+    const boardColumn = parsedCell % 3;
+    if (currentPlayer === player1) {
+      board[boardRow][boardColumn] = "X";
+    } else {
+      board[boardRow][boardColumn] = "O";
     }
-    
+    cells[parsedCell].innerHTML = board[boardRow][boardColumn];
+    isWin();
+    isTie();
+    switchPlayers();
+    console.log(board);
+  };
 
-    return {
-        switchPlayers, //done
-        currentPlayer, //done
-        board,   //done
-        displayBoard, //done
-        displayWinner,
-        resetBoard,
-        isWin,
-        isTie,
-        addMark,
+  const isWin = () => {
+    WIN_CONDITIONS.forEach((element) => {
+      const [firstElement, secondElement, thirdElement] = element;
+      if (
+        (board[firstElement[0]][firstElement[1]] ===
+          board[secondElement[0]][secondElement[1]]) ===
+          board[thirdElement[0][thirdElement[1]]] &&
+        board[secondElement[0]][secondElement[1]] !== ""
+      ) {
+        return true;
+      }
+    });
+    return false;
+  };
+
+  const isTie = () => {
+    board.forEach((row) => {
+      if (!row.includes("") && !isWin()) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  const displayWinner = () => {};
+
+  const resetBoard = () => {};
+
+  const switchPlayers = () => {
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
     }
-}
+  };
 
-
-gameFlow.displayBoard()
+  return {
+    addMark,
+    board, //done
+    currentPlayer, //done
+    // displayWinner,
+    isTie,
+    isWin,
+    // resetBoard,
+    switchPlayers, //done
+  };
+};
+gameFlow();
